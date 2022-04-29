a0c75e8fc847dbf3f3dd482ef386c4a2692bdf87
zerinrc <joaquinzero@gmail.com>
2022-04-29 21:03:10 +0200

    installation scripts

--- install.sh
+++ install.sh
@@ -23,9 +23,10 @@ ln -s /data/solaredge-dbus-meter/service /service/solaredge-dbus-meter
 
 
 # add install-script to rc.local to be ready for firmware update
-filename=/data/rc.local
+filename=/data/rcS.local
 if [ ! -f $filename ]
 then
+    echo "entra"
     touch $filename
     chmod 755 $filename
     echo "#!/bin/bash" >> $filename
